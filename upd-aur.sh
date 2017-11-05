52de3245ef47ce686e8dba541bb4f67886f6985a
FabioLolix <fabio.lolix@gmail.com>
2017-11-05 02:04:33 +0100

    Moving folders

--- upd-aur.sh
+++ upd-aur.sh
@@ -1,154 +1,148 @@
 #! /bin/bash
 
-VAR1=dvdae-bin
-cd $PWD/$VAR1
-    makepkg --printsrcinfo > .SRCINFO
+VAR1=alsaplayer-git
+ cd $PWD/$VAR1
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR1
 cd ..
 
 
-VAR2=gimagereader
+VAR2=brother-dcpt700w
 cd $PWD/$VAR2
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,gimagereader.install} ~/Dev/Github/AUR/$VAR2
+    rsync {PKGBUILD,.SRCINFO,fix_lp.patch,LICENSE} ~/Dev/Github/AUR/$VAR2
 cd ..
 
 
-#VAR2=gtkspellmm
-#cd $PWD/$VAR2
-#    makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR2
-#cd ..
-
-
-VAR3=headset-bin
+VAR3=corebird-git
 cd $PWD/$VAR3
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR3
 cd ..
 
 
-VAR4=libupnpp
+
+VAR4=dvdae-bin
 cd $PWD/$VAR4
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR4
 cd ..
 
 
-VAR5=linux-test-project
+VAR5=gimagereader
 cd $PWD/$VAR5
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR5
+    rsync {PKGBUILD,.SRCINFO,gimagereader.install} ~/Dev/Github/AUR/$VAR5
 cd ..
 
 
-VAR6=neobarok
+VAR6=gmusicbrowser-git
 cd $PWD/$VAR6
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR6
 cd ..
 
 
-VAR7=phockup
-cd $PWD/$VAR7
-    makepkg --printsrcinfo > .SRCINFO
+VAR7=gource-git
+ cd $PWD/$VAR7
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR7
 cd ..
 
 
-VAR8=phoronix-test-suite-milestone
-cd $PWD/$VAR8
-    makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,phoronix-test-suite-milestone.install} ~/Dev/Github/AUR/$VAR8
+VAR8=gravit-designer-bin
+ cd $PWD/$VAR8
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR8
 cd ..
 
 
-VAR9=polo
+VAR9=green-recorder
 cd $PWD/$VAR9
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR9
 cd ..
 
 
-#VAR10=polo-bin
+#VAR10=gtkspellmm
 #cd $PWD/$VAR10
 #    makepkg --printsrcinfo > .SRCINFO
 #    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR10
 #cd ..
 
 
-VAR11=polo-donation-plugins
+VAR11=headset-bin
 cd $PWD/$VAR11
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR11
 cd ..
 
 
-VAR12=qcomicbook
-cd $PWD/$VAR12
-    makepkg --printsrcinfo > .SRCINFO
+VAR12=kitty
+ cd $PWD/$VAR12
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR12
 cd ..
 
-VAR13=quickhash-gui-bin
-cd $PWD/$VAR13
-    makepkg --printsrcinfo > .SRCINFO
+
+VAR13=libav-no-libs
+ cd $PWD/$VAR13
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR13
 cd ..
 
 
-VAR14=upplay
-cd $PWD/$VAR14
-    makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR14
-cd ..
-
-
-VAR15=toptracker
-cd $PWD/$VAR15
-    makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,toptracker.desktop,LICENSE} ~/Dev/Github/AUR/$VAR15
-cd ..
-
-
-#VAR16=brother-dcpt700w
-#cd $PWD/$VAR16
+#VAR14=libupnpp
+#cd $PWD/$VAR14
 #    makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO,fix_lp.patch,LICENSE} ~/Dev/Github/AUR/$VAR16
+#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR14
 #cd ..
 
 
-VAR17=qjackrcd
-cd $PWD/$VAR17
+VAR15=linux-test-project
+cd $PWD/$VAR15
     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR15
+cd ..
+
+
+VAR16=linux-test-project-git
+ cd $PWD/$VAR16
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR16
+cd ..
+
+VAR17=madrigal-git
+ cd $PWD/$VAR17
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR17
 cd ..
 
 
-VAR18=qomp
-cd $PWD/$VAR18
-    makepkg --printsrcinfo > .SRCINFO
+VAR18=mstreamexpress-bin
+ cd $PWD/$VAR18
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR18
 cd ..
 
 
-VAR19=qomp-git
+VAR19=neobarok
 cd $PWD/$VAR19
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR19
 cd ..
 
 
-VAR20=gmusicproxy
-cd $PWD/$VAR20
-    makepkg --printsrcinfo > .SRCINFO
+VAR20=obsidian-2-theme-git
+ cd $PWD/$VAR20
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR20
 cd ..
 
 
-VAR21=gmusicbrowser-git
-cd $PWD/$VAR21
-    makepkg --printsrcinfo > .SRCINFO
+VAR21=obsidian-icon-theme-git
+ cd $PWD/$VAR21
+     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR21
 cd ..
 
@@ -160,26 +154,127 @@ cd $PWD/$VAR22
 cd ..
 
 
-VAR23=playbluray
+VAR23=phockup
 cd $PWD/$VAR23
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,playBluRay.sh,playBluRay-icon.png} ~/Dev/Github/AUR/$VAR23
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR23
 cd ..
 
-VAR24=voltra
+
+VAR24=phoronix-test-suite-milestone
 cd $PWD/$VAR24
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR24
+    rsync {PKGBUILD,.SRCINFO,phoronix-test-suite-milestone.install} ~/Dev/Github/AUR/$VAR24
 cd ..
 
-VAR25=corebird-git
-cd $PWD/$VAR25
-    makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR25
-cd ..
 
-VAR26=green-recorder
+#VAR25=playbluray
+#cd $PWD/$VAR25
+#    makepkg --printsrcinfo > .SRCINFO
+#    rsync {PKGBUILD,.SRCINFO,playBluRay.sh,playBluRay-icon.png} ~/Dev/Github/AUR/$VAR25
+#cd ..
+
+
+VAR26=polo
 cd $PWD/$VAR26
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR26
+cd ..
+
+
+#VAR27=polo-bin
+#cd $PWD/$VAR27
+#    makepkg --printsrcinfo > .SRCINFO
+#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR27
+#cd ..
+
+
+VAR28=polo-donation-plugins
+cd $PWD/$VAR28
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR28
+cd ..
+
+
+VAR29=ptxconf-git
+ cd $PWD/$VAR29
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR29
+cd ..
+
+
+VAR30=qcomicbook
+cd $PWD/$VAR30
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR30
+cd ..
+
+
+VAR31=qjackrcd
+cd $PWD/$VAR31
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR31
+cd ..
+
+
+VAR32=qomp
+cd $PWD/$VAR32
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR32
+cd ..
+
+
+VAR33=qomp-git
+cd $PWD/$VAR33
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR33
+cd ..
+
+
+VAR34=quickhash-gui-bin
+cd $PWD/$VAR34
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR34
+cd ..
+
+
+VAR35=sacd-decoder-bzr
+ cd $PWD/$VAR35
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR35
+cd ..
+
+
+VAR36=selene-media-converter
+ cd $PWD/$VAR36
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR36
+cd ..
+
+
+VAR37=sonerezh
+ cd $PWD/$VAR37
+     makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR37
+cd ..
+
+
+VAR38=toptracker
+cd $PWD/$VAR38
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO,toptracker.desktop,LICENSE} ~/Dev/Github/AUR/$VAR38
+cd ..
+
+
+#VAR39=upplay
+#cd $PWD/$VAR39
+#    makepkg --printsrcinfo > .SRCINFO
+#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR39
+#cd ..
+
+
+VAR40=voltra
+cd $PWD/$VAR40
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR40
 cd ..
