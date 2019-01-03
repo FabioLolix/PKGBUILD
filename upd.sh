f4ac9270f23542adcee82ed92d13072c742b42a1
FabioLolix <fabio.lolix@gmail.com>
2019-01-03 17:27:17 +0100

    [gotop] add

--- upd.sh
+++ upd.sh
@@ -1,582 +1,466 @@
 #! /bin/bash
 
-VAR1=alsaplayer-git
- cd $PWD/$VAR1
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR1
-cd ..
+BASE=~/Dev/Github/PKGBUILD
 
+DEST=~/Dev/AUR
 
-#VAR2=brother-dcpt700w
-#cd $PWD/$VAR2
-#    makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO,fix_lp.patch,LICENSE,brother-dcpt700w.install} ~/Dev/Github/AUR/$VAR2
-#cd ..
-
-
-#VAR3=corebird-git
-#cd $PWD/$VAR3
-#    makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR3
-#cd ..
-
-
-VAR4=dvdae-bin
-cd $PWD/$VAR4
+VAR=dvdae-bin
+cd "$BASE/$VAR"
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR4
-cd ..
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
 
 
-VAR5=gimagereader
-cd $PWD/$VAR5
+VAR=gmusicbrowser-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR5
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR6=gmusicbrowser-git
-cd $PWD/$VAR6
+VAR=gource-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR6
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR7=gource-git
- cd $PWD/$VAR7
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR7
-cd ..
-
-
-#VAR8=gravit-designer-bin
-# cd $PWD/$VAR8
-#     makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO,gravit-designer.desktop,gravit-designer.png,LICENSE} ~/Dev/Github/AUR/$VAR8
-#cd ..
-
-
-VAR9=green-recorder
-cd $PWD/$VAR9
+VAR=green-recorder
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR9
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR10=gtkspellmm
-cd $PWD/$VAR10
+VAR=headset-bin
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR10
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR11=headset-bin
-cd $PWD/$VAR11
+VAR=libav-no-libs
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR11
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR13=libav-no-libs
- cd $PWD/$VAR13
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR13
-cd ..
-
-
-VAR14=libupnpp
-cd $PWD/$VAR14
+VAR=libupnpp
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR14
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR15=linux-test-project
-cd $PWD/$VAR15
+VAR=madrigal-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR15
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR16=linux-test-project-git
- cd $PWD/$VAR16
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR16
-cd ..
-
-VAR17=madrigal-git
- cd $PWD/$VAR17
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR17
-cd ..
-
-
-VAR18=mstreamexpress-bin
- cd $PWD/$VAR18
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR18
-cd ..
-
-
-VAR19=neobarok
-cd $PWD/$VAR19
+VAR=mstreamexpress-bin
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,neobarok-LICENSE} ~/Dev/Github/AUR/$VAR19
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR20=obsidian-2-theme-git
- cd $PWD/$VAR20
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR20
-cd ..
-
-
-VAR21=obsidian-icon-theme-git
- cd $PWD/$VAR21
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR21
-cd ..
-
-
-VAR22=openage-git
-cd $PWD/$VAR22
+VAR=neobarok
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR22
-cd ..
+    rsync {PKGBUILD,.SRCINFO,neobarok-LICENSE} $DEST/$VAR
 
 
-VAR23=phockup
-cd $PWD/$VAR23
+VAR=obsidian-2-theme-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR23
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR24=phoronix-test-suite-milestone
-cd $PWD/$VAR24
+VAR=obsidian-icon-theme-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,phoronix-test-suite-milestone.install} ~/Dev/Github/AUR/$VAR24
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
+
+VAR=openage-git
+cd $BASE/$VAR
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
+
+VAR=phockup
+cd $BASE/$VAR
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
+
+VAR=phoronix-test-suite-milestone
+cd $BASE/$VAR
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO,phoronix-test-suite-milestone.install} $DEST/$VAR
 
 
 VAR26=polo
-cd $PWD/$VAR26
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,*.patch} ~/Dev/Github/AUR/$VAR26
-cd ..
+    rsync {PKGBUILD,.SRCINFO,*.patch} $DEST/$VAR
 
 
 VAR=polo-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR27=polo-bin
-cd $PWD/$VAR27
+VAR=polo-bin
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR27
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR28=polo-donation-plugins
-cd $PWD/$VAR28
+VAR=polo-donation-plugins
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR28
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR29=ptxconf-git
- cd $PWD/$VAR29
+VAR=ptxconf-git
+cd $BASE/$VAR
      makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,ptxconf.desktop,ptxconf.sh} ~/Dev/Github/AUR/$VAR29
-cd ..
+    rsync {PKGBUILD,.SRCINFO,ptxconf.desktop,ptxconf.sh} $DEST/$VAR
 
 
-VAR30=qcomicbook
-cd $PWD/$VAR30
+VAR=qcomicbook
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR30
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR31=qjackrcd
-cd $PWD/$VAR31
+VAR=qjackrcd
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR31
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR32=qomp
-cd $PWD/$VAR32
+VAR=quickhash-gui-bin
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,*.patch} ~/Dev/Github/AUR/$VAR32
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR33=qomp-git
-cd $PWD/$VAR33
+VAR=sacd-decoder-bzr
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR33
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR34=quickhash-gui-bin
-cd $PWD/$VAR34
-    makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR34
-cd ..
-
-
-VAR35=sacd-decoder-bzr
- cd $PWD/$VAR35
+VAR=selene-media-converter
+cd $BASE/$VAR
      makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR35
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR36=selene-media-converter
- cd $PWD/$VAR36
-     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR36
-cd ..
-
-
-VAR38=toptracker
-cd $PWD/$VAR38
+VAR=toptracker
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,toptracker.desktop,toptracker-LICENSE} ~/Dev/Github/AUR/$VAR38
-cd ..
+    rsync {PKGBUILD,.SRCINFO,toptracker.desktop,toptracker-LICENSE} $DEST/$VAR
 
 
-VAR39=upplay
-cd $PWD/$VAR39
+VAR=upplay
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR39
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR40=voltra
-cd $PWD/$VAR40
+VAR=voltra
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,voltra.desktop,COPYRIGHT,voltra.png,*.html} ~/Dev/Github/AUR/$VAR40
-cd ..
+    rsync {PKGBUILD,.SRCINFO,voltra.desktop,COPYRIGHT,voltra.png,*.html} $DEST/$VAR
 
 
-VAR41=kitty-git
-cd $PWD/$VAR41
+VAR=kitty-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR41
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR43=textosaurus
-cd $PWD/$VAR43
+VAR=textosaurus
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR43
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR44=qimgv
-cd $PWD/$VAR44
+VAR=qimgv
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR44
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR45=delft-icon-theme
-cd $PWD/$VAR45
+VAR=delft-icon-theme
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR45
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR46=openwmail-bin
-cd $PWD/$VAR46
+VAR=openwmail-bin
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR46
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR47=elisa-git
-cd $PWD/$VAR47
+VAR=elisa-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR47
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR48=mirage2iso
-cd $PWD/$VAR48
+VAR=mirage2iso
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR48
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR49=faience-ng-icon-theme
-cd $PWD/$VAR49
+VAR=faience-ng-icon-theme
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR49
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR51=perl-webservice-musicbrainz
-cd $PWD/$VAR51
+VAR=perl-webservice-musicbrainz
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR51
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR52=inxi
-cd $PWD/$VAR52
+VAR=inxi
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR52
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR53=twin-git
-cd $PWD/$VAR53
+VAR=twin-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR53
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-#VAR54=gimagereader-beta
-#cd $PWD/$VAR54
-#    makepkg --printsrcinfo > .SRCINFO
-#    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR54
-#cd ..
-
-
-VAR55=textosaurus-git
-cd $PWD/$VAR55
+VAR=textosaurus-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR55
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR57=simple-scan-git
-cd $PWD/$VAR57
+VAR=simple-scan-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR57
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
-VAR58=rclone-git
-cd $PWD/$VAR58
+VAR=rclone-git
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR58
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=hashit
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=imageburner
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=playmymusic
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=playmyvideos
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=showmypictures
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=inxi-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=mdbook
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=mdbook-bin
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=mdbook-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=phoronix-test-suite
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=ambiance-rw-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=abcde-musicbrainz
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=brasero-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=easyssh
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=easyssh-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=hashit-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=imageburner-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=playmymusic-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=playmyvideos-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=showmypictures-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=sparky-dashboard-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=yarock-mpv
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=gnome-video-arcade
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=launchar-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
 
 
 VAR=strawberry
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=strawberry-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=strawberry-full-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=strawberry-lite
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=flare-game
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=flare-engine
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=mauikit-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=vvave-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
+
 
 VAR=squeezelite
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,conffile,service,squeezelite.install} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO,conffile,service,squeezelite.install} $DEST/$VAR
+
 
 VAR=squeezelite-git
-cd $PWD/$VAR
+cd $BASE/$VAR
     makepkg --printsrcinfo > .SRCINFO
-    rsync {PKGBUILD,.SRCINFO,conffile,service,squeezelite.install} ~/Dev/Github/AUR/$VAR
-cd ..
+    rsync {PKGBUILD,.SRCINFO,conffile,service,squeezelite.install} $DEST/$VAR
+
+
+VAR=gotop
+cd $BASE/$VAR
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} $DEST/$VAR
