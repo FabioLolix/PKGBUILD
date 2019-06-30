2b6f00f7aecbc5ad6fd18e4d0e05a6cdaabf7394
FabioLolix <fabio.lolix@gmail.com>
2019-07-01 00:46:44 +0200

    [vuescan-bin xbxoxdrv-git] update

--- 00_AUR_fix/upd.sh
+++ 00_AUR_fix/upd.sh
@@ -13,3 +13,23 @@ VAR=mjpg-streamer-git
 cd "$BASE/$VAR"
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
+
+VAR=gccxml-git
+cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
+
+VAR=sunxi-tools-git
+cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
+
+VAR=vuescan-bin
+cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
+
+VAR=xboxdrv-git
+cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR"
