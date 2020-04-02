5a3de269ba5dd699c9c4099d1fdb2f406a32f8d7
FabioLolix <fabio.lolix@gmail.com>
2020-04-02 10:41:36 +0200

    bumps & updates

--- u.sh
+++ u.sh
@@ -4,6 +4,18 @@ BASE=~/Dev/Github/PKGBUILD/
 DEST=~/Dev/AUR
 
 
+VAR2=b
+  VAR=brasero-git
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+  VAR=baobab-git
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+
 VAR2=e
   VAR=easyssh
   cd "$BASE/$VAR"
@@ -16,8 +28,42 @@ VAR2=e
     rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
 
 
+VAR2=g
+  VAR=guitar
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+  VAR=guitar-git
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+
+VAR2=n
+  VAR=nemu
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+
 VAR2=p
+  VAR=phoronix-test-suite
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
   VAR=phoronix-test-suite-milestone
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+  VAR=polo
+  cd "$BASE/$VAR"
+    makepkg --printsrcinfo > .SRCINFO
+    rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
+
+  VAR=polo-git
   cd "$BASE/$VAR"
     makepkg --printsrcinfo > .SRCINFO
     rsync {PKGBUILD,.SRCINFO} "$DEST/$VAR2/$VAR"
